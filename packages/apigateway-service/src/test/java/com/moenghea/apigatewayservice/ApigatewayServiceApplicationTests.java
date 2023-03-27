6f272cc61a644da2d68dbd7441cc4935a9271cf8
cheomuk <pkw9815@naver.com>
2023-03-27 19:31:36 +0900

    Add Back-End Application

---
+++ packages/apigateway-service/src/test/java/com/moenghea/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moenghea.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ packages/profile-service/src/main/java/com/meonghea/profileservice/ProfileServiceApplication.java
@@ -0,0 +1,17 @@
+package com.meonghea.profileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileServiceApplication.class, args);
+	}
+
+}
