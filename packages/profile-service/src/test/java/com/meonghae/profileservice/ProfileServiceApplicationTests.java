b4b8fba9dd7479fa4141a96a124713ec2e80b143
CJ-Park <king01286@naver.com>
2023-03-29 20:35:11 +0900

    Services Version Matching / Modify Package Name

---
+++ packages/profile-service/src/test/java/com/meonghae/profileservice/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meonghae.profileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
