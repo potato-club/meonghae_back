aafff763f8120cd37cbc89ae5f6c88bdee773bc7
cheomuk <pkw9815@naver.com>
2023-04-26 20:54:35 +0900

    Add Spring Scheduling

--- packages/user-service/src/main/java/com/meonghae/userservice/repository/UserRepository.java
+++ packages/user-service/src/main/java/com/meonghae/userservice/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.meonghae.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -12,4 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     boolean existsByEmailAndDeleted(String email, boolean deleted);
+    List<User> findByDeletedIsTrueAndModifiedDateBefore(LocalDateTime localDateTime);
 }
