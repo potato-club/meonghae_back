763623b8567e7d11984cd682a337ea0898ea588d
CJ-Park <king01286@naver.com>
2023-05-05 15:30:38 +0900

    #1 [fix] Spring actuator와 Swagger 동시 사용시 충돌 문제 수정

--- packages/community-service/src/main/java/com/meonghae/communityservice/Config/Swagger2Config.java
+++ packages/community-service/src/main/java/com/meonghae/communityservice/Config/Swagger2Config.java
@@ -1,9 +1,19 @@
 package com.meonghae.communityservice.Config;
 
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,6 +23,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.function.Predicate;
 
 @Configuration
@@ -20,6 +33,39 @@ import java.util.function.Predicate;
 @ComponentScan(basePackages = {"com.meonghae"})
 @Import(BeanValidatorPluginsConfiguration.class)
 public class Swagger2Config {
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping
+            (WebEndpointsSupplier webEndpointsSupplier,
+             ServletEndpointsSupplier servletEndpointsSupplier,
+             ControllerEndpointsSupplier controllerEndpointsSupplier,
+             EndpointMediaTypes endpointMediaTypes,
+             CorsEndpointProperties corsProperties,
+             WebEndpointProperties webEndpointProperties,
+             Environment environment)
+    {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(
+                webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(
+                endpointMapping, webEndpoints, endpointMediaTypes,
+                corsProperties.toCorsConfiguration(),
+                new EndpointLinksResolver(allEndpoints, basePath),
+                shouldRegisterLinksMapping, null);
+    }
+
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties,
+                                               Environment environment, String basePath)
+    {
+        return webEndpointProperties.getDiscovery().isEnabled()
+                && (StringUtils.hasText(basePath) ||
+                ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+    }
     @Bean(name = "defaultApi")
     public Docket defaultApi() {
         return new Docket(DocumentationType.SWAGGER_2)
