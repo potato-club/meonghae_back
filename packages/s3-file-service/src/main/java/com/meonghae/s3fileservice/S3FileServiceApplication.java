cc7fe9d3222d54000d77ac508dca681bc90165a9
cheomuk <pkw9815@naver.com>
2023-05-27 22:26:33 +0900

    Add S3 File Service Application

---
+++ packages/s3-file-service/src/main/java/com/meonghae/s3fileservice/S3FileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.meonghae.s3fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3FileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3FileServiceApplication.class, args);
+	}
+
+}
