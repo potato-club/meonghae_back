135c79df4d9d5629659e467b54d6176810f82c86
CJ-Park <king01286@naver.com>
2023-06-12 20:00:45 +0900

    Add Headers annotation on FeignClient

--- packages/community-service/src/main/java/com/meonghae/communityservice/Client/S3ServiceClient.java
+++ packages/community-service/src/main/java/com/meonghae/communityservice/Client/S3ServiceClient.java
@@ -5,6 +5,7 @@ import com.meonghae.communityservice.Config.FeignUploadConfig;
 import com.meonghae.communityservice.Dto.S3Dto.S3RequestDto;
 import com.meonghae.communityservice.Dto.S3Dto.S3ResponseDto;
 import com.meonghae.communityservice.Dto.S3Dto.S3UpdateDto;
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @FeignClient(name = "s3-file-service", configuration = {FeignHeaderConfig.class, FeignUploadConfig.class})
+@Headers("Content-Type: application/json")
 public interface S3ServiceClient {
     @GetMapping("/files")
     List<S3ResponseDto> getImages(S3RequestDto requestDto);
