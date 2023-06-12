09909982ba43bd03a82fa9dcb236ade94ae5fbb1
CJ-Park <king01286@naver.com>
2023-06-12 18:37:40 +0900

    Fix image check method

--- packages/community-service/src/main/java/com/meonghae/communityservice/Service/BoardService.java
+++ packages/community-service/src/main/java/com/meonghae/communityservice/Service/BoardService.java
@@ -136,7 +136,7 @@ public class BoardService {
         boardRepository.delete(board);
     }
 
-    private void imageCheck(Board saveBoard, List<MultipartFile> images, int reuseSize) {
+    public void imageCheck(Board saveBoard, List<MultipartFile> images, int reuseSize) {
         if(saveBoard.getType() == BoardType.MISSING && images.size() - reuseSize > 5) {
             throw new BoardException(BAD_REQUEST, "실종 게시글 사진은 최대 5개까지 업로드 가능합니다.");
         } else if(images.size() - reuseSize > 3) {
