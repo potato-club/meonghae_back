88f93109bb183d82c9087b649678f49a107648b9
cheomuk <pkw9815@naver.com>
2023-07-25 16:47:04 +0900

    Revise Error Handler

--- packages/apigateway-service/src/main/java/com/moenghae/apigatewayservice/error/MyWebExceptionHandler.java
+++ packages/apigateway-service/src/main/java/com/moenghae/apigatewayservice/error/MyWebExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.moenghae.apigatewayservice.error;
 
+import com.moenghae.apigatewayservice.jwt.JwtExpiredException;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
@@ -29,7 +30,7 @@ public class MyWebExceptionHandler implements ErrorWebExceptionHandler {
             errorCode = ErrorCode.INVALID_JWT_TOKEN.getCode();
         } else if (ex.getClass() == UnsupportedJwtException.class) {
             errorCode = ErrorCode.UNSUPPORTED_JWT_TOKEN.getCode();
-        } else if (ex.getClass() == ExpiredJwtException.class) {
+        } else if (ex.getClass() == JwtExpiredException.class) {
             errorCode = ErrorCode.JWT_TOKEN_EXPIRED.getCode();
         } else if (ex.getClass() == IllegalArgumentException.class) {
             errorCode = ErrorCode.EMPTY_JWT_CLAIMS.getCode();
