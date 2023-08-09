df2d5a834dcc32989edb2c53813720ce84b15964
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-08-10 06:58:32 +0900

    revise code

--- packages/profile-service/src/main/java/com/meonghae/profileservice/dto/calendar/ScheduleRequestDTO.java
+++ packages/profile-service/src/main/java/com/meonghae/profileservice/dto/calendar/ScheduleRequestDTO.java
@@ -2,11 +2,13 @@ package com.meonghae.profileservice.dto.calendar;
 
 import com.meonghae.profileservice.entity.Pet;
 import com.meonghae.profileservice.entity.Schedule;
+import com.meonghae.profileservice.enumCustom.ScheduleCycleType;
 import com.meonghae.profileservice.enumCustom.ScheduleType;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Calendar;
@@ -15,15 +17,19 @@ import java.util.Calendar;
 public class ScheduleRequestDTO {
   @ApiModelProperty(notes = "반려동물 id", example = "LONG", required = true)
   private Long petId;
-  @ApiModelProperty(notes = "백신 지정할 타입", example = "DHPPL, Coronavirus 등 string값")
+  @ApiModelProperty(notes = "일정 지정 타입", example = "합의된 일정 or Custom(유저 커스텀)")
   private ScheduleType scheduleType;
-  @ApiModelProperty(notes = "일정 날짜와 시간", example = "2023-01-01THH:mm", required = true)
-  private LocalDateTime scheduleTime;
-  @ApiModelProperty(notes = "알림 지정 시간 ", example = "2023-01-01THH:mm")
-  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
-  private LocalDateTime alarmTime;
   @ApiModelProperty(notes = "자동 반복 여부", example = "T or F")
   private boolean hasRepeat;
+  @ApiModelProperty(notes = "반복시간 타입설정", example = "Month or Day")
+  private ScheduleCycleType cycleType;
+  @ApiModelProperty(notes = "반복주기 설정", example = "Month or Day")
+  private int cycle;
+  @ApiModelProperty(notes = "일정 날짜와 시간", example = "2023-01-01THH:mm", required = true)
+  private LocalDateTime scheduleTime;
+  @ApiModelProperty(notes = "알림 지정 시간", example = "2023-01-01THH:mm")
+  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime alarmTime;
   @ApiModelProperty(notes = "일정 정보", example = "예방주사 맞는 날", required = true)
   private String text;
 
--- packages/profile-service/src/main/java/com/meonghae/profileservice/repository/RecurringScheduleRepository.java
+++ packages/profile-service/src/main/java/com/meonghae/profileservice/repository/RecurringScheduleRepository.java
@@ -1,7 +1,7 @@
-package com.meonghae.profileservice.repository;
-
-import com.meonghae.profileservice.entity.RecurringSchedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecurringScheduleRepository extends JpaRepository<RecurringSchedule,Long> {
-}
+//package com.meonghae.profileservice.repository;
+//
+//import com.meonghae.profileservice.entity.RecurringSchedule;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface RecurringScheduleRepository extends JpaRepository<RecurringSchedule,Long> {
+//}
