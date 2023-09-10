cbfd6906ab92441c10c9df820eeeac3a6ca0687e
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-09-10 18:49:17 +0900

    revise FCM logic

--- packages/profile-service/src/main/java/com/meonghae/profileservice/controller/PetController.java
+++ packages/profile-service/src/main/java/com/meonghae/profileservice/controller/PetController.java
@@ -16,7 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.net.util.SubnetUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-
+//
 import javax.servlet.http.HttpServletRequest;
 
 
