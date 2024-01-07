4302db91208a67584cae4ef0d1abcec592c8c81e
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2024-01-08 05:58:34 +0900

    [revise] #2 월별 일정 Enum 수정

--- packages/profile-service/src/main/java/com/meonghae/profileservice/enumCustom/ScheduleType.java
+++ packages/profile-service/src/main/java/com/meonghae/profileservice/enumCustom/ScheduleType.java
@@ -10,16 +10,20 @@ public enum ScheduleType {
     //유저
     BirthDay(1,"특별한 날인 오늘, 생일 축하드려요!",12),
     PetBirthDay(2,"반려동물의 생일인 오늘, 행복한 시간되세요!",12),
-    //강아지
-    DHPPL(11,"혼합백신",12),
-    Coronavirus(12,"코로나장염",12),
-    KennelCough(13,"전염성기관지염",12),
+    //예방접종
+    DHPPL(11,"강아지혼합예방주사",12),
+    CVRP(12,"고양이혼합예방주사",12),
+    Coronavirus(13,"코로나바이러스성 장염",12),
     Rabies(14, "광견병",6),
-    HeartWorm(15,"심장사상충",1),
-    //고양이
-    CVRP(21,"혼합예방주사",12),
-    FelineLeukemia(22,"고양이 백혈병",12),
-    FIP(23,"전염성 복막염",12);
+    KennelCough(15,"기관지염",12),
+    FelineLeukemia(16,"고양이 백혈병",12),
+    FIP(17,"전염성 복막염",12),
+    //구충제
+    HeartWorm(21,"심장사상충",1),
+    InternalParasitic(22,"내부기생충",1),
+    ExternalParasites(23,"외부기생충",2),
+    //건강검진
+    HealthScreenings(31,"건강검진",12);
 
 
     private final int key;
