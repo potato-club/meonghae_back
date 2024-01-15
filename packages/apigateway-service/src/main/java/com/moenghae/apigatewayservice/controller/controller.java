5822c7fcb33bcc1b5e1d842d959e06517748e98a
cheomuk <pkw9815@naver.com>
2024-01-15 17:55:20 +0900

    [Fix] Add Jwt Token Validate API

--- packages/apigateway-service/src/main/java/com/moenghae/apigatewayservice/controller/controller.java
+++ packages/apigateway-service/src/main/java/com/moenghae/apigatewayservice/controller/controller.java
@@ -1,12 +1,25 @@
 package com.moenghae.apigatewayservice.controller;
 
+import com.moenghae.apigatewayservice.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class controller {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
     @GetMapping("/health-check")
     public String check() {
         return "health-checking";
+    }
+
+    @GetMapping("/check")
+    public boolean checkToken(ServerHttpRequest request) {
+        String accessToken = jwtTokenProvider.resolveAccessToken(request);
+        return jwtTokenProvider.validateToken(accessToken);
     }
 }
--- packages/user-service/src/main/java/com/meonghae/userservice/controller/UserController.java
+++ packages/user-service/src/main/java/com/meonghae/userservice/controller/UserController.java
@@ -6,10 +6,8 @@ import com.meonghae.userservice.service.Interface.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
