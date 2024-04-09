06b6e8f64238a2370e75b7c138049187331d18d6
cheomuk <pkw9815@naver.com>
2024-04-09 17:50:47 +0900

    #32 [Refactoring] Success SignUp API Test.

--- packages/user-service/src/test/java/com/meonghae/userservice/controller/UserControllerTest.java
+++ packages/user-service/src/test/java/com/meonghae/userservice/controller/UserControllerTest.java
@@ -1,6 +1,9 @@
 package com.meonghae.userservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meonghae.userservice.dto.user.UserRequest;
 import com.meonghae.userservice.mock.FakePetServiceClient;
+import com.meonghae.userservice.mock.FakeS3ServiceClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -13,6 +16,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -28,8 +32,14 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean(FakePetServiceClient.class)
     private FakePetServiceClient fakePetServiceClient;
+
+    @MockBean(FakeS3ServiceClient.class)
+    private FakeS3ServiceClient fakeS3ServiceClient;
 
     @Test
     void 사용자는_로그인을_진행할_수_있다() throws Exception {
@@ -52,5 +62,47 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.responseCode").value("200_OK"))
                 .andExpect(header().exists("Authorization"))
                 .andExpect(header().exists("RefreshToken"));
+    }
+
+    @Test
+    void 회원이_아닐_경우_회원_가입을_진행할_수_있다() throws Exception {
+        // given
+        String email = "test1@test.com";
+        String androidId = "test-android-123-456-780";
+        String fcmToken = "test-FCMToken-123-456-780";
+
+        UserRequest request = UserRequest.builder()
+                .email(email)
+                .nickname("Test-User")
+                .age(25)
+                .birth("20000101")
+                .build();
+
+        // when
+        // then
+        mockMvc.perform(
+                    get("/login")
+                         .queryParam("email", email)
+                         .contentType(MediaType.APPLICATION_JSON)
+                         .header("androidId", androidId)
+                         .header("FCMToken", fcmToken)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value(email))
+                .andExpect(jsonPath("$.responseCode").value("201_CREATED"));
+
+        mockMvc.perform(
+                    post("/signup")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("email", email)
+                        .param("nickname", request.getNickname())
+                        .param("age", String.valueOf(25))
+                        .param("birth", "20000101")
+                        .header("androidId", androidId)
+                        .header("FCMToken", fcmToken)
+            )
+            .andExpect(status().isOk())
+            .andExpect(header().exists("Authorization"))
+            .andExpect(header().exists("RefreshToken"));
     }
 }
