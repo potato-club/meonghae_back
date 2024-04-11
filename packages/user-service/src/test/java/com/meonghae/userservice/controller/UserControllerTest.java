76a32e833c85a12ca8fee3a1acd28c7f1e5460b2
cheomuk <pkw9815@naver.com>
2024-04-12 04:24:18 +0900

    #32 [Refactoring] Success viewMyPage API Test.

--- packages/user-service/src/test/java/com/meonghae/userservice/controller/UserControllerTest.java
+++ packages/user-service/src/test/java/com/meonghae/userservice/controller/UserControllerTest.java
@@ -1,44 +1,111 @@
 package com.meonghae.userservice.controller;
 
 import com.meonghae.userservice.dto.user.UserRequest;
-import com.meonghae.userservice.mock.FakePetServiceClient;
-import com.meonghae.userservice.mock.FakeS3ServiceClient;
+import com.meonghae.userservice.service.client.feign.PetServiceClient;
+import com.meonghae.userservice.service.client.feign.S3ServiceClient;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
+import javax.sql.DataSource;
 import java.util.Objects;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 @TestPropertySource(properties = {"spring.config.location = classpath:application-test-user.yml"})
-@SqlGroup({
-        @Sql(value = "/sql/controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
-        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-})
 public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private FakePetServiceClient fakePetServiceClient;
+    private PetServiceClient petServiceClient;
 
     @MockBean
-    private FakeS3ServiceClient fakeS3ServiceClient;
+    private S3ServiceClient s3ServiceClient;
+
+    @BeforeAll
+    public static void setUp(@Autowired DataSource dataSource) {
+        // 스프링 컨텍스트가 로드된 후, 한 번만 실행되는 초기화 블록
+        ResourceDatabasePopulator pt = new ResourceDatabasePopulator();
+        pt.addScript(new ClassPathResource("/sql/controller-test-data.sql"));
+        pt.execute(dataSource);
+    }
+
+    @AfterAll
+    public static void cleanUp(@Autowired DataSource dataSource) {
+        // 모든 테스트가 완료된 후, 데이터 정리
+        ResourceDatabasePopulator pt = new ResourceDatabasePopulator();
+        pt.addScript(new ClassPathResource("/sql/delete-all-data.sql"));
+        pt.execute(dataSource);
+    }
+
+    @Test
+    void 회원이_아닐_경우_회원_가입을_진행할_수_있다() throws Exception {
+        // given
+        String email = "test1@test.com";
+        String androidId = "test-android-123-456-780";
+        String fcmToken = "test-FCMToken-123-456-780";
+
+        UserRequest userRequest = UserRequest.builder()
+                .email(email)
+                .nickname("Test-User")
+                .age(25)
+                .birth("20000101")
+                .build();
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        // when
+        // then
+        mockMvc.perform(
+                        get("/login")
+                                .queryParam("email", email)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("androidId", androidId)
+                                .header("FCMToken", fcmToken)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value(email))
+                .andExpect(jsonPath("$.responseCode").value("201_CREATED"));
+
+        mockMvc.perform(
+                        post("/signup")
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .param("email", email)
+                                .param("nickname", userRequest.getNickname())
+                                .param("age", String.valueOf(25))
+                                .param("birth", "20000101")
+                                .header("androidId", androidId)
+                                .header("FCMToken", fcmToken)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().exists("Authorization"))
+                .andExpect(header().exists("RefreshToken"));
+    }
 
     @Test
     void 사용자는_로그인을_진행할_수_있다() throws Exception {
@@ -51,58 +118,17 @@ public class UserControllerTest {
         // then
         mockMvc.perform(
                         get("/login")
-                                .queryParam("email", email)
+                                .param("email", email)
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .header("androidId", androidId)
                                 .header("FCMToken", fcmToken)
                 )
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.email").isEmpty())
                 .andExpect(jsonPath("$.responseCode").value("200_OK"))
                 .andExpect(header().exists("Authorization"))
                 .andExpect(header().exists("RefreshToken"));
-    }
-
-    @Test
-    void 회원이_아닐_경우_회원_가입을_진행할_수_있다() throws Exception {
-        // given
-        String email = "test1@test.com";
-        String androidId = "test-android-123-456-780";
-        String fcmToken = "test-FCMToken-123-456-780";
-
-        UserRequest request = UserRequest.builder()
-                .email(email)
-                .nickname("Test-User")
-                .age(25)
-                .birth("20000101")
-                .build();
-
-        // when
-        // then
-        mockMvc.perform(
-                    get("/login")
-                         .queryParam("email", email)
-                         .contentType(MediaType.APPLICATION_JSON)
-                         .header("androidId", androidId)
-                         .header("FCMToken", fcmToken)
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.email").value(email))
-                .andExpect(jsonPath("$.responseCode").value("201_CREATED"));
-
-        mockMvc.perform(
-                    post("/signup")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("email", email)
-                        .param("nickname", request.getNickname())
-                        .param("age", String.valueOf(25))
-                        .param("birth", "20000101")
-                        .header("androidId", androidId)
-                        .header("FCMToken", fcmToken)
-            )
-            .andExpect(status().isOk())
-            .andExpect(header().exists("Authorization"))
-            .andExpect(header().exists("RefreshToken"));
     }
 
     @Test
@@ -119,33 +145,32 @@ public class UserControllerTest {
                 .birth("20000101")
                 .build();
 
+        ResultActions resultActions = mockMvc.perform(get("/login")
+                                        .queryParam("email", email)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .header("androidId", androidId)
+                                        .header("FCMToken", fcmToken));
+
         // when
-        // then
-        mockMvc.perform(
-                        get("/login")
-                                .queryParam("email", email)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .header("androidId", androidId)
-                                .header("FCMToken", fcmToken)
-                )
-                .andExpect(status().isOk())
+        MvcResult mvcResult = resultActions.andExpect(status().isOk())
                 .andExpect(jsonPath("$.responseCode").value("200_OK"))
                 .andExpect(header().exists("Authorization"))
                 .andExpect(header().exists("RefreshToken"))
-                .andDo(result -> {
-                    String accessToken = Objects.requireNonNull(result.getResponse().getHeader("Authorization")).substring(7);
-                    String refreshToken = Objects.requireNonNull(result.getResponse().getHeader("RefreshToken")).substring(7);
+                .andReturn();
 
-                    mockMvc.perform(
-                                    get("/mypage")
-                                            .header("Authorization", accessToken)
-                                            .header("RefreshToken", refreshToken)
-                            )
-                            .andExpect(status().isOk())
-                            .andExpect(jsonPath("$.email").value(email))
-                            .andExpect(jsonPath("$.nickname").value(request.getNickname()))
-                            .andExpect(jsonPath("$.age").value(request.getAge()))
-                            .andExpect(jsonPath("$.birth").value(request.getBirth()));
-                });
+        String accessToken = Objects.requireNonNull(mvcResult.getResponse().getHeader("Authorization")).substring(7);
+        String refreshToken = Objects.requireNonNull(mvcResult.getResponse().getHeader("RefreshToken")).substring(7);
+
+        // then
+        mockMvc.perform(
+                        get("/mypage")
+                                .header("Authorization", accessToken)
+                                .header("RefreshToken", refreshToken)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value(email))
+                .andExpect(jsonPath("$.nickname").value(request.getNickname()))
+                .andExpect(jsonPath("$.age").value(request.getAge()))
+                .andExpect(jsonPath("$.birth").value("2000-01-01"));
     }
 }
