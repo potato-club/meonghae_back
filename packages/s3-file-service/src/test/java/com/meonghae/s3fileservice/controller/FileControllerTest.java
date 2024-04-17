6643493af8a3e85541fd0d7497cd7b498f627046
cheomuk <pkw9815@naver.com>
2024-04-17 13:14:39 +0900

    #32 [Refactoring] Add Update Test in FileControllerTest.

--- packages/s3-file-service/src/test/java/com/meonghae/s3fileservice/controller/FileControllerTest.java
+++ packages/s3-file-service/src/test/java/com/meonghae/s3fileservice/controller/FileControllerTest.java
@@ -3,25 +3,35 @@ package com.meonghae.s3fileservice.controller;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.meonghae.s3fileservice.domain.enums.EntityType;
-import com.meonghae.s3fileservice.dto.FileUser;
+import com.meonghae.s3fileservice.dto.*;
 import com.meonghae.s3fileservice.mock.FakeS3URL;
 import com.meonghae.s3fileservice.service.port.FileRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockPart;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.sql.DataSource;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -37,6 +47,9 @@ public class FileControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private FileRepository fileRepository;
 
     @Autowired
@@ -47,6 +60,22 @@ public class FileControllerTest {
         FakeS3URL s3URL = new FakeS3URL();
         given(amazonS3.putObject(any(PutObjectRequest.class))).willReturn(new PutObjectResult());
         given(amazonS3.getUrl(any(), any())).willReturn(s3URL.getAmazonS3Url("test-s3", any()));
+    }
+
+    @BeforeAll
+    public static void setUp(@Autowired DataSource dataSource) {
+        // 스프링 컨텍스트가 로드된 후, 한 번만 실행되는 초기화 블록
+        ResourceDatabasePopulator pt = new ResourceDatabasePopulator();
+        pt.addScript(new ClassPathResource("/sql/controller-test-data.sql"));
+        pt.execute(dataSource);
+    }
+
+    @AfterAll
+    public static void cleanUp(@Autowired DataSource dataSource) {
+        // 모든 테스트가 완료된 후, 데이터 정리
+        ResourceDatabasePopulator pt = new ResourceDatabasePopulator();
+        pt.addScript(new ClassPathResource("/sql/delete-all-data.sql"));
+        pt.execute(dataSource);
     }
 
     @Test
@@ -67,11 +96,153 @@ public class FileControllerTest {
         mockMvc.perform(
                         MockMvcRequestBuilders.multipart("/files/users")
                                 .file(mock)
-                                .param("email", request.getEmail())
-                                .param("entityType", request.getEntityType().toString())
+                                .part(new MockPart("data", objectMapper.writeValueAsBytes(request)))
                                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .accept(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
                 .andExpect(status().isOk());
+
+        FileUserResponse file = fileRepository.getUserProfile(request.getEmail());
+
+        assertThat(file.getFileName()).isNotNull();
+        assertThat(file.getFileUrl()).isNotNull();
+        assertThat(file.getEmail()).isEqualTo(request.getEmail());
+        assertThat(file.getEntityType()).isEqualTo(request.getEntityType());
+    }
+
+    @Test
+    void 유저_서비스_외_다른_서비스의_사진을_업로드한다() throws Exception {
+        // given
+        MockMultipartFile mock = new MockMultipartFile("files",
+                "test1.png",
+                "image/png",
+                "test-file1".getBytes(StandardCharsets.UTF_8));
+
+        MockMultipartFile mock2 = new MockMultipartFile("files",
+                "test2.png",
+                "image/png",
+                "test-file2".getBytes(StandardCharsets.UTF_8));
+
+        FileRequest request = FileRequest.builder()
+                .entityType(EntityType.BOARD)
+                .entityId(1L)
+                .build();
+
+        // when
+        // then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.multipart("/files")
+                                .file(mock)
+                                .file(mock2)
+                                .part(new MockPart("data", objectMapper.writeValueAsBytes(request)))
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        List<FileResponse> files = fileRepository.getFileList(request);
+
+        for (FileResponse file : files) {
+            assertThat(file.getFileName()).isNotNull();
+            assertThat(file.getFileUrl()).isNotNull();
+            assertThat(file.getEntityType()).isEqualTo(request.getEntityType());
+            assertThat(file.getTypeId()).isEqualTo(request.getEntityId());
+        }
+    }
+
+    @Test
+    void 유저_서비스의_사진을_업데이트한다() throws Exception {
+        // given
+        List<FileUpdate> list = new ArrayList<>();
+
+        MockMultipartFile mock = new MockMultipartFile("files",
+                "test1-1.png",
+                "image/png",
+                "test-file1-1".getBytes(StandardCharsets.UTF_8));
+
+        FileUserResponse file = fileRepository.getUserProfile("test@test.com");
+
+        FileUpdate request = FileUpdate.builder()
+                .fileName(file.getFileName())
+                .fileUrl(file.getFileUrl())
+                .entityType(EntityType.USER)
+                .email("test@test.com")
+                .deleted(true)
+                .build();
+
+        list.add(request);
+
+        // when
+        // then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.multipart("/files")
+                                .file(mock)
+                                .part(new MockPart("dataList", objectMapper.writeValueAsBytes(list)))
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        FileUserResponse response = fileRepository.getUserProfile(request.getEmail());
+
+        assertThat(response.getFileName()).isNotEqualTo(file.getFileName());
+        assertThat(response.getFileUrl()).isNotEqualTo(file.getFileUrl());
+        assertThat(response.getEmail()).isEqualTo(request.getEmail());
+        assertThat(response.getEntityType()).isEqualTo(request.getEntityType());
+    }
+
+    @Test
+    void 유저_서비스_외_다른_서비스의_사진을_업데이트한다() throws Exception {
+        // given
+        List<FileUpdate> list = new ArrayList<>();
+
+        MockMultipartFile mock = new MockMultipartFile("files",
+                "test1-1.png",
+                "image/png",
+                "test-file1-1".getBytes(StandardCharsets.UTF_8));
+
+        MockMultipartFile mock2 = new MockMultipartFile("files",
+                "test2-1.png",
+                "image/png",
+                "test-file2-1".getBytes(StandardCharsets.UTF_8));
+
+        List<FileResponse> files = fileRepository.getFileList(new FileRequest(EntityType.BOARD, 1L));
+
+        for (FileResponse fileResponse : files) {
+            FileUpdate request = FileUpdate.builder()
+                    .fileName(fileResponse.getFileName())
+                    .fileUrl(fileResponse.getFileUrl())
+                    .entityType(fileResponse.getEntityType())
+                    .entityId(fileResponse.getTypeId())
+                    .deleted(true)
+                    .build();
+
+            list.add(request);
+        }
+
+        // when
+        // then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.multipart("/files")
+                                .file(mock)
+                                .file(mock2)
+                                .part(new MockPart("dataList", objectMapper.writeValueAsBytes(list)))
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        List<FileResponse> responseList = fileRepository.getFileList(new FileRequest(EntityType.BOARD, 1L));
+
+        for (int i = 0; i < responseList.size(); i++) {
+            assertThat(responseList.get(i).getFileName()).isNotEqualTo(list.get(i).getFileName());
+            assertThat(responseList.get(i).getFileUrl()).isNotEqualTo(list.get(i).getFileUrl());
+            assertThat(responseList.get(i).getEntityType()).isEqualTo(list.get(i).getEntityType());
+            assertThat(responseList.get(i).getTypeId()).isEqualTo(list.get(i).getEntityId());
+        }
     }
 }
